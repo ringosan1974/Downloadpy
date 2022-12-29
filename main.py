f2ab3f085cf2a8e0e6896d679f8abe80492f016c
ringosan1974 <wacyakun@gmail.com>
2022-12-30 01:39:23 +0900

    クラス化

--- main.py
+++ main.py
@@ -11,25 +11,49 @@ header = {
 }
 
 
-def download_image(soup, class_name):
-    links = soup.find_all("img", attrs={"class":f"{class_name}"})
-
-    for i in links:
-        src = urllib.request.Request(url=i.attrs['src'], headers=header)
-        filename = os.path.basename(src.full_url)
-        data = urllib.request.urlopen(src).read()
-        with open(filename, mode="wb") as f:
-            f.write(data)
-        time.sleep(0.5)
+class Downloader():
+    def __init__(self, url, header=header, dirname=""):
+        self.url = url
+        self.header = header
+        self.dirname = dirname
 
 
-def parse_html(url, headers=header):
-    html = get_html(url, headers=header)
-    return BeautifulSoup(html, 'html.parser')
+    def download_image(self, attrs):
+        soup = self._parse_html()
+        links = soup.find_all("img", attrs=attrs)
+
+        for i in links:
+            src = urllib.request.Request(url=i.attrs['src'], headers=self.header)
+            filename = os.path.basename(src.full_url)
+            data = urllib.request.urlopen(src).read()
+            if len(self.dirname) > 0:
+                self.dirname += "\\"
+            with open(f"{self.dirname}{filename}", mode="wb") as f:
+                f.write(data)
+            time.sleep(1)
 
 
-def get_html(url, headers=header):
-    req = urllib.request.Request(url=url, headers=header)
-    http_obj = urllib.request.urlopen(req)
-    data = http_obj.read()
-    return data.decode('utf-8')
+    def _parse_html(self):
+        html = self._get_html()
+        return BeautifulSoup(html, 'html.parser')
+
+
+    def _get_html(self):
+        req = urllib.request.Request(url=self.url, headers=self.header)
+        http_obj = urllib.request.urlopen(req)
+        data = http_obj.read()
+        return data.decode('utf-8')
+
+
+if __name__ == "__main__":
+    url = input("url :")
+    head = input("head :")
+    value = input("value :")
+    attrs = {head:value}
+    downloader = Downloader(url, dirname="D:\\images\\other\\")
+    try:
+        downloader.download_image(attrs)
+    except ValueError as e:
+        print("正しくURLを入力してください\n", e)
+    except urllib.request.HTTPError as e:
+        print("HTTPError:", e)
